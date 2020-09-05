013a2ade65c18b919b100a824e8d406cd7401880
EsraaAlaaOmar <esraa.safa55@gmail.com>
2020-09-05 21:51:29 +0200

    Prepare for deployment

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use('/api/posts', require('./routers/api/posts'));
 
     //set static folder
     app.use(express.static('client/build'))
+    app.get('*',(req,res) => {
+       res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    })
  }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`esraa + ${{ PORT }}`));
